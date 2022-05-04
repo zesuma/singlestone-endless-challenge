be372be69c131ad3736d64fe0ab82d8c6ef7951d
zesuma <trevercumming@icloud.com>
2022-05-03 20:24:58 -0400

    add protypes to hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,36 @@
+import PropTypes from 'prop-types'
 
-function Hero() {
+function Hero() { 
+    const heroSubTitle = 'New Games & Accessories'
+    const heroTitle = `Monthly packages. 
+    Excitement delivered daily.`
+    const heroDescription = "What's the best way to shop for the latest video games and peripherals? How about never shopping at all? You'll get new stuff on your doorstep - every month."
+    const heroButton = 'Get Started'
     return (
         <section className="hero">
             <div className="container">
                 <div className="hero__box">
-                    <h2 className="hero__subtitle">New Games &amp; Accessories</h2>
+                    <h2 className="hero__subtitle">{heroSubTitle}</h2>
                     <h1 className="hero__title">
-                        Monthly packages.<br/>
-                        Excitement delivered daily.
+                        {heroTitle}
                     </h1>
                     <p className="hero__description">
-                        What's the best way to shop for the latest video games and peripherals? 
-                        How about never shopping at all? 
-                        You'll get new stuff on your doorstep - every month.
+                        {heroDescription}
                     </p>
                     <a href="https://endless.com" className="btn hero__button">
-                        Get Started
+                        {heroButton}
                     </a>
                 </div>
             </div>
         </section>
     )
+}
+
+Hero.propTypes = {
+    heroSubTitle: PropTypes.string,
+    heroTitle: PropTypes.string,
+    heroDescription: PropTypes.string,
+    heroButton: PropTypes.string,
 }
 
 export default Hero
